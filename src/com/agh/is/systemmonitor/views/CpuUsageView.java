c629ce53916c87caae48bdc87e58d62503743a9a
Adrian Kremski <adrian.kremski@gmail.com>
2012-11-21 07:53:37 +0100

    History bug fixed

--- src/com/agh/is/systemmonitor/views/CpuUsageView.java
+++ src/com/agh/is/systemmonitor/views/CpuUsageView.java
@@ -49,7 +49,7 @@ public class CpuUsageView extends LinearLayout {
 				activity.runOnUiThread(new Runnable() {
 					@Override
 					public void run() {
-						new ShowStatisticsTask(activity.getBaseContext(), dialogManager, agent, 
+						new ShowStatisticsTask(activity, dialogManager, agent, 
 								new ServerParametersBuilder().login(SystemMonitorActivity.login)
 								.password(SystemMonitorActivity.password)
 								.host(SystemMonitorActivity.host).recordId(String.valueOf(agent.getId()))).execute(new Void[]{});
